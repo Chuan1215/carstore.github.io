7bcf2d30ba309367a4f546f8941f49198c0a94f8
Chuan1215 <148991160+Chuan1215@users.noreply.github.com>
2024-07-07 16:04:28 +0800

    Add files via upload

--- carstore/settings.py
+++ carstore/settings.py
@@ -133,8 +133,6 @@ STATIC_URL = "static/"
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
